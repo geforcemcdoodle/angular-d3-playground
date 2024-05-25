2aec68bb1faf4158b1365c02258ef5ee247175e2
geforcemcdoodle <h0091891@gmail.com>
2024-05-25 15:45:10 +0200

    fail: https://stackoverflow.com/questions/45846147/angular-2-how-to-have-an-svg-in-app-component-and-a-circle-as-a-child-comp

--- src/app/d3/visuals/shared/node-visual.component.ts
+++ src/app/d3/visuals/shared/node-visual.component.ts
@@ -2,9 +2,9 @@ import { Component, Input } from '@angular/core';
 import { Node } from '../../models/node';
 
 @Component({
-  selector: 'nodeVisual',
+  selector: '[nodeVisual]',
   standalone: true,
-  templateUrl: './node-visual.component.html',  
+  templateUrl: './node-visual.component.svg',  
 })
 export class NodeVisualComponent {
   @Input('node') node!: Node;
