f07f98cb761f60705a4ad6e8bf215aca4da1624f
geforcemcdoodle <h0091891@gmail.com>
2024-05-25 14:04:50 +0200

    add: loop over two nodes but each resides in its own svg element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NodeVisualComponent } from '../d3/visuals/shared/node-visual.component';
 import { Node } from '../d3/models/node';
 
+
 @Component({
   selector: 'app-base',
   standalone: true,
@@ -10,9 +11,12 @@ import { Node } from '../d3/models/node';
   styleUrl: './base.component.css'
 })
 export class BaseComponent {
-  node: Node;
+  nodes: Node[];
   
   constructor() {
-    this.node = { r:15, x: 100, y:100, id: 1};
+    this.nodes = [
+      {r:15, x: 100, y:100, id: 1},
+      {r:15, x: 200, y:100, id: 2}
+    ];
   }
 }
